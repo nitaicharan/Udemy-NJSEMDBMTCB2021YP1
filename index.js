88aceb09983857c604c7c0204bcacc06c2152a63
Nitai Charan <nitaicharan@gmail.com>
2020-11-13 00:00:00 -0300

    10. Reading and Writing Files Asynchronously

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('ERROR! *')
 
-console.log(textIn);
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
 
-const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
+            fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err => console.log('Your file has been written :)'));
+        });
+    });
+});
 
-fs.writeSync('./txt/output.txt', textOut);
-
-console.log('File written!')
+console.log('Will read file!');
